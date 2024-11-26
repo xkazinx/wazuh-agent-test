8b9fa14786c07b99471e2f0829b854c2a49afc80
Marcel Kemp Muñoz <marcel.kemp@wazuh.com>
2024-11-26 16:43:24 +0100

    feat: backport WMI and WUA API integration
    Merge pull request #26706 from wazuh/enhancement/25766-develop-integration-with-wmi-api-to-retrieve-installed-windows-updates
    
    Develop Integration with WMI and WUA APIs to Retrieve Installed Windows Updates

--- src/common/data_provider/tests/sysInfoWin/sysInfoWin_test.h
+++ src/common/data_provider/tests/sysInfoWin/sysInfoWin_test.h
@@ -15,6 +15,9 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
+#define WMI_WUA_DLL
+#include "utilsWrapperWin.hpp"
+
 class SysInfoWinTest : public ::testing::Test
 {
     protected:
@@ -25,5 +28,22 @@ class SysInfoWinTest : public ::testing::Test
         void SetUp() override;
         void TearDown() override;
 };
+
+
+class MockComHelper : public IComHelper
+{
+    public:
+        MOCK_METHOD(HRESULT, CreateWmiLocator, (IWbemLocator*& pLoc), (override));
+        MOCK_METHOD(HRESULT, ConnectToWmiServer, (IWbemLocator* pLoc, IWbemServices*& pSvc), (override));
+        MOCK_METHOD(HRESULT, SetProxyBlanket, (IWbemServices* pSvc), (override));
+        MOCK_METHOD(HRESULT, ExecuteWmiQuery, (IWbemServices* pSvc, IEnumWbemClassObject*& pEnumerator), (override));
+        MOCK_METHOD(HRESULT, CreateUpdateSearcher, (IUpdateSearcher*& pUpdateSearcher), (override));
+        MOCK_METHOD(HRESULT, GetTotalHistoryCount, (IUpdateSearcher* pUpdateSearcher, LONG& count), (override));
+        MOCK_METHOD(HRESULT, QueryHistory, (IUpdateSearcher* pUpdateSearcher, IUpdateHistoryEntryCollection*& pHistory,  LONG& count), (override));
+        MOCK_METHOD(HRESULT, GetCount, (IUpdateHistoryEntryCollection* pHistory, LONG& count), (override));
+        MOCK_METHOD(HRESULT, GetItem, (IUpdateHistoryEntryCollection* pHistory, LONG index, IUpdateHistoryEntry** pEntry), (override));
+        MOCK_METHOD(HRESULT, GetTitle, (IUpdateHistoryEntry* pEntry, BSTR& title), (override));
+};
+
 
 #endif //_SYSINFO_WIN_TEST_H
